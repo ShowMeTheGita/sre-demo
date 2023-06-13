e213a2a315d14ff3cd24a5569db43c859f4aed78
Guilherme Ferreira da Silva <guilherme.ferreira.silva@celfocus.com>
2023-06-13 16:45:41 +0100

    change default dashboards timeframe to last 5m

--- resources/webapp-entrypoint.sh
+++ resources/webapp-entrypoint.sh
@@ -2,5 +2,5 @@
 # Entrypoint script for webapp container startup
 # Restarts sshd and starts the nodejs express webapp
 # tail keeps the container running so we can use ansible for start/stopping the webapp without crashing the container
-sudo /sbin/rc-service sshd restart && usr/local/bin/node /app/index.js > /dev/null 2>&1 &
+sudo /sbin/rc-service sshd restart && /usr/local/bin/node /app/index.js > /dev/null 2>&1 &
 tail -f /dev/null
